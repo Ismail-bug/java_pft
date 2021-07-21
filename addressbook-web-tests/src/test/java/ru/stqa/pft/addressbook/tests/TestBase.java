992f32352241b07c245c282cbfac747b3c0c3681
Ismail-bug <forzoom01@mail.ru>
2021-07-21 10:42:46 +0300

    Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
-
+Logger logger = LoggerFactory.getLogger(TestBase.class);
     public static ApplicationManager app;
 
     static {
@@ -28,6 +34,14 @@ public class TestBase {
     public void tearDown() throws Exception {
         app.stop();
 
+    }
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p){
+        logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.info("Stop test" + m.getName());
     }
 
 }
