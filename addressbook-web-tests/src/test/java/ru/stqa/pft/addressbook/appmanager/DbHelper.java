eb6bd956c7f2a087261186d154b809246d9f5257
Ismail-bug <forzoom01@mail.ru>
2021-07-25 16:48:02 +0300

    hibernate 5.4.12

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/DbHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/DbHelper.java
@@ -15,14 +15,15 @@ import java.util.List;
 public class DbHelper {
     private final SessionFactory sessionFactory;
 
-    public DbHelper(){
+    public DbHelper() {
         // A SessionFactory is set up once for an application!
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure() // configures settings from hibernate.cfg.xml
                 .build();
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
     }
+
     public Groups groups() {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
@@ -31,10 +32,11 @@ public class DbHelper {
         session.close();
         return new Groups(result);
     }
-    public Contacts contacts(){
+
+    public Contacts contacts() {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        List<ContactData> result = session.createQuery( "from ContactData" ).list();
+        List<ContactData> result = session.createQuery("from ContactData").list();
         session.getTransaction().commit();
         session.close();
         return new Contacts(result);
